3221916e2204df55840f635a6cbe78dac2e156a0
walterthesmart <nwaugowalter@gail.com>
2025-01-02 20:47:00 +0100

    first  solana program

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -6,11 +6,50 @@ declare_id!("AYcauDAZF2DNpsiWzxtrrHUfhh6n3aETeSfjKX9w34QH");
 pub mod calculator {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        let calculator= &mut ctx.axcounts.calculator;
+        calculator.gretting = init_message;
+        Ok({})
+    }
+
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
+        Ok({})
+    }
+
+    pub fn sub(ctx: Context<Subtraction>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 - num2;
+        Ok({})
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+
+    #[account(init, payer=user, space=264)]
+    pub calculator: Account<'info, Calculator>,
+
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Addition<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Subtraction<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[account]
+pub struct Calculator {
+    pub gretting: String,
+    result: i64,
+}
+
